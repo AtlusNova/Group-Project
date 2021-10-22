c708a92b3193fe2c570c98297e5aa9618bbcc693
AlexanderOr <aortwine@ltu.edu>
2021-10-22 13:06:10 -0400

    Key additions
    Added animation and light to key

---
+++ Assets/Scripts/gameLose.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameLose : MonoBehaviour
+{
+    
+    public GameObject loseUI;
+    public GameObject explosionEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+
+        // only if you enter the "Enemy" object this activates 
+        if (other.gameObject.name == "Enemy")
+        {
+
+            Vector3 particleSpawnPoint = other.transform.position;
+            Instantiate(explosionEffect, particleSpawnPoint, Quaternion.identity);
+            Destroy(GameObject.Find("Player"));
+
+            loseUI.SetActive(true);
+        }
+
+    }
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        print("you quit the game");
+
+    }
+}
